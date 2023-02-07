83e689b09834bccc4538832f35e1e45ecb19a9be
ulvertrom <ulvertrom@gmail.com>
2023-02-08 00:29:44 +0400

    3 progs added

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char letterC = 99, letterH = 104, letterA = 97, letterR = 114;
+        System.out.println(letterC + "" + letterH + "" + letterA + "" + letterR);
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-//         first = 10_000_000_000L;
-//         second = 'a';
-//         third = 5 > 9;
-//         forth = 12.90434323324342;
-//         fifth = 12.3F;
-//         sixth = 1;
-//         seventh = 129;
+         long first = 10_000_000_000L; System.out.println(first);
+         char second = 'a'; System.out.println(second);
+         boolean third = 5 > 9; System.out.println(third);
+         double forth = 12.90434323324342; System.out.println(forth);
+         float fifth = 12.3F; System.out.println(fifth);
+         byte sixth = 1; System.out.println(sixth);
+         short seventh = 129; System.out.println(seventh);
     }
 }
